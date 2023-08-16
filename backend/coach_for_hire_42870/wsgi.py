8d8570ccb6d5fcfe0d0d5f72027f1e6fe03e7a19
Crowdbotics <team@crowdbotics.com>
2023-08-16 12:04:14 +0000

    Initial commit.

---
+++ backend/coach_for_hire_42870/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for coach_for_hire_42870 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coach_for_hire_42870.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "coach-for-hire-42870",
+  url: "https://coach-for-hire-42870.botics.co",
+  api: "https://coach-for-hire-42870.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
